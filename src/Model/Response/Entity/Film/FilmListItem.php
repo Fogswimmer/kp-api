a4bec8d7e376732590acd3463a7a7fdc6c9d742a
Nebelschwimmer <nebelschwimmer@gmail.com>
2025-07-25 11:01:28 +0300

    refactor: added new fields to mappers

--- src/Model/Response/Entity/Film/FilmListItem.php
+++ src/Model/Response/Entity/Film/FilmListItem.php
@@ -59,16 +59,19 @@ class FilmListItem
     {
         return $this->id;
     }
+
     public function setId(int $id): static
     {
         $this->id = $id;
 
         return $this;
     }
+
     public function getName(): string
     {
         return $this->name;
     }
+
     public function setName(string $name): static
     {
         $this->name = $name;
